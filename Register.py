ac4d7a3d9d8c55a78553eb70fbd2439ca389fc2b
St. Jimmy <titaniumcobaltspork@gmail.com>
2019-10-31 21:16:58 -0700

    Added flask forms

--- Register.py
+++ Register.py
@@ -1,6 +1,11 @@
+# NOTE
+# Most of this is just to test if Register works since the main app py file isn't up yet
+
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -8,3 +13,28 @@ class RegistrationForm(FlaskForm):
     confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
+
+#   def validate_username(self, username):
+#       user = User.query.filter_by(username=username.data).first()
+#       if user is not None:
+#           raise ValidationError('Username already taken')
+
+#   def validate_email(self, email):
+#       user = User.query.filter_by(email=email.data).first()
+#       if user is not None:
+#           raise ValidationError('Email address already taken')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some-key'
+
+
+@app.route('/')
+def register():
+    form = RegistrationForm()
+
+    return render_template('Register.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
