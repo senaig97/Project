de68902d9f58d92f3f11597fce34f1368c724b62
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-11-03 12:42:30 -0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    # Code related to log in to go here
-    print('Log in')
+    class LoginForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
 
 class EditCredsForm(FlaskForm):
     newUsername = StringField('New Username')
     newPassword = PasswordField('New Password', validators=[DataRequired()])
     submit = SubmitField('Confirm')
+   
+class SplitForm(FlaskForm):
+    amount = IntegerField('Amount')
+    people = IntegerField('People')
