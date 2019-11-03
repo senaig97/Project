e5184a9953d871b64d6ba78065525368cb0b59bc
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-11-03 13:20:03 -0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,47 @@
 from app import SmartSplitApp
-from flask import render_template
-from app.forms import EditCredsForm
+from flask import request, session, redirect, url_for, render_template, flash, g
+from flask import app
+from app.form import EditCredsForm, LoginForm, SplitForm, RegistrationForm
+
+@SmartSplitApp.route("/")
 
 
-@SmartSplitApp.route('/')
-def logIn():
-    # code related to log in to go here
-    username = None
+@SmartSplitApp.route("/home")
+def home():
+    return render_template("Home.html")
+
+
+@SmartSplitApp.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('Register.html', form=form)
+
+
+@SmartSplitApp.route("/login", methods=["GET", "POST"])
+def login():
+    g.selected_tab = "login"
+
+    form = None
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            error = "Invalid login credentials"
+        elif request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid login credentials"
+        else:
+            session["logged_in"] = True
+            flash("You are logged in")
+            if session.get("project"):
+                return redirect(url_for("home"))
+            return redirect(url_for("projects"))
+
+    return render_template("LogIn.html", form=form)
+
+
+@SmartSplitApp.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("projects"))
 
 
 @SmartSplitApp.route('/editCredentials')
