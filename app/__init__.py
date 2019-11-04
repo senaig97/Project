3b37e5e7ee873e91feeede6f07a345f8d4635bb8
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-11-04 00:24:19 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from config import Config
+from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 SmartSplitApp = Flask(__name__)
 SmartSplitApp.config.from_object(Config)
 
 db = SQLAlchemy(SmartSplitApp)
+
+login = LoginManager(SmartSplitApp)
+
+login.login_view = 'login'
 
 from app import routes, models
