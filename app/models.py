3884b8ed46796fbb492576001d6a0fd60d305af4
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-11-04 00:26:07 -0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,25 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def set_password(self , password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self , password):
+        return check_password_hash(self.password , password)
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
