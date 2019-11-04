d9f54adf9af77952fb0c9bf0a80299d568d24b85
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-11-04 00:25:27 -0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField, validators
-# from wtforms.validators import DataRequired
-
-
-class RegistrationForm(FlaskForm):
-    # code for registration to go here
-    print('Please register')
+from wtforms import StringField, PasswordField, IntegerField, FloatField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, NumberRange
+from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[validators.DataRequired()])
+    username = StringField('Username', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
 
 
 class EditCredsForm(FlaskForm):
     newUsername = StringField('New Username')
-    newPassword = PasswordField('New Password', validators=[
-        validators.data_required(),
-        validators.equal_to('confirm', message='Passwords must match')
-    ])
-    submit = SubmitField('Repeat Password')
+    newPassword = PasswordField('New Password', validators=[DataRequired()])
+    submit = SubmitField('Confirm')
 
 
 class SplitForm(FlaskForm):
-    amount = IntegerField('Amount')
-    people = IntegerField('People')
+    amount = FloatField('cost')
+    people = IntegerField('people', validators=[NumberRange(min=1)])
+    splitbill = FloatField('splitbill')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
