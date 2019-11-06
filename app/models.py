2c445ef10838f75842285e5aebb9dacc1a9acddd
St. Jimmy <titaniumcobaltspork@gmail.com>
2019-11-05 23:06:04 -0800

    Changed __repr__ to print username.

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password , password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.body)
+        return '<User {}>'.format(self.username)
 
 @login.user_loader
 def load_user(id):
