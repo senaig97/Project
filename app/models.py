e27ce6dfe3574ef3e0da26fce40e08ac368030c3
St. Jimmy <titaniumcobaltspork@gmail.com>
2019-11-05 23:29:25 -0800

    Change password variable to password_hash.

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
 
     def set_password(self , password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self , password):
         return check_password_hash(self.password , password)
