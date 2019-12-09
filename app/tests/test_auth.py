0de0e250de14be341cdc0fc59569272b20521a54
senaig97 <54563300+senaig97@users.noreply.github.com>
2019-12-09 13:14:31 -0800

    Update test_auth.py

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -3,6 +3,7 @@ from app.models import User
 
 
 @pytest.fixture(scope='module')
+
 def test_add_user_to_db(db):
     user1 = User(username='john', password='test')
     db.session.add(user1)
@@ -15,3 +16,14 @@ def test_get_login_page(client):
     assert response.status_code == 200
     assert b'Username' in response.data
     assert b'Password' in response.data
+
+    
+def test_valid_register(client, db):
+
+    response = client.post('/signup',
+                                data=dict(username='testing', email='testing@testing.com', password='testing', confirm='testing'),
+                                follow_redirects=True)
+    assert response.status_code == 200
+    assert b"You are now logged in!" in response.data
+    assert b'Hi !' in response.data
+    assert b'Log out' in response.data
