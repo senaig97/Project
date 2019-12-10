81472c14bc0e56538fdcb851867311273246595f
St. Jimmy <titaniumcobaltspork@gmail.com>
2019-12-10 03:06:11 -0800

    Added and updated more test cases.

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -2,12 +2,27 @@ import pytest
 from app.models import User
 
 
-@pytest.fixture(scope='module')
 def test_add_user_to_db(db):
-    user1 = User(username='john', password='test')
-    db.session.add(user1)
+    user = User(username='john', email='john@john.com')
+    user.set_password('password')
+    db.session.add(user)
     db.session.commit()
-    assert len(User.query.all()) == 1
+    assert len(User.query.all()) == 2
+
+
+def test_nonauth_homepage(client):
+    response = client.get('/home')
+    assert response.status_code == 200
+    assert b'<a href="http://127.0.0.1:5000/login">Log In</a>' in response.data
+
+
+def test_auth_homepage(client):
+    response = client.post('/login', data=dict(username='testificate', password='password'), follow_redirects=True)
+    assert response.status_code == 200
+
+    response = client.get('/home')
+    assert response.status_code == 200
+    assert b'<a href="http://127.0.0.1:5000/logout">Log Out</a>' in response.data
 
 
 def test_get_login_page(client):
@@ -20,29 +35,16 @@ def test_get_login_page(client):
 def test_valid_register(client, db):
     response = client.post('/register',
                            data=dict(username='testing', email='testing@testing.com', password='testing',
-                                     confirm='testing'),
+                                     confirmpassword='testing'),
                            follow_redirects=True)
     assert response.status_code == 200
-    assert b"You are now logged in!" in response.data
-    assert b'Hi !' in response.data
-    assert b'Log out' in response.data
+    assert b"Username" in response.data
+    assert b'Password' in response.data
 
 
 def test_change_password(client):
-    client.get('/register', follow_redirects=True)
-    client.register('foo fighter', 'Foo', 'Foo', 'foofighter@foo.com')
-    response = client.post('/editCredentials', data=dict(password='Fighter'), follow_redirects=True)
+    client.post('/login', data=dict(username='testificate', password='password'), follow_redirects=True)
+    response = client.post('/editCredentials', data=dict(password='Fighter', confirmPassword='Fighter'),
+                           follow_redirects=True)
     assert response.status_code == 200
     # self.assertIn(b'Password has been updated!', response.data)
-    # self.assertIn(b'User Profile', response.data)
-
-
-def test_authorized_links(client):
-    response = client.post('/register',
-                           data=dict(username='testificate', email='testificate@villager.com', password='testing',
-                                     confirm='testing'),
-                           follow_redirects=True)
-    assert b'Log Out' in response.data
-    assert b'Edit Credentials' in response.data
-    assert b'Even Split' in response.data
-    assert b'History' in response.data
--- app/tests/test_split.py
+++ app/tests/test_split.py
@@ -1,13 +1,9 @@
 import pytest
 from app.models import User
 
-
-@pytest.fixture(scope='module')
-def test_evensplit(client):
-    client.get('/register', follow_redirects=True)
-    client.register('foo fighter', 'Foo', 'Foo', 'foofighter@foo.com')
-    response = client.post('/even', data=dict(cost = 20, people = 4, comment='testing'), follow_redirects=True)
-    assert b'5.0' in response.data
+def test_evensplit(client, db):
+    response = client.post('/evensplit', data=dict(cost = 20, people = 4, comment='testing'))
+    assert b'$5.0' in response.data
 
 def test_history(client):
     client.get('/register', follow_redirects=True)
